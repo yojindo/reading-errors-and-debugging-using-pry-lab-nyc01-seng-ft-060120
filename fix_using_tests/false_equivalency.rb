f68b698373e2ca1c2c49be0317921168f6d81808
Jake Kwak <jakejaekwak@gmail.com>
2020-05-11 20:49:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,9 +15,9 @@ end
 
 def selection(num)
   if num = 1
-     puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     puts "HAM HAM HAM IN MY TUMMY"
+    "YUM YUM MUNCH MUNCH MUNCH"
+  if num = 2
+    "HAM HAM HAM IN MY TUMMY"
   end
 end
 
